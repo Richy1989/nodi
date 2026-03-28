38e15aa528f3c73c93eb040d9dba9e0461898290
Richard Leopold <richy.leopold@outlook.com>
2026-03-28 15:17:07 +0100

    enter adds new checklist item

--- nodiWeb/wwwroot/js/nodi.js
+++ nodiWeb/wwwroot/js/nodi.js
@@ -1,5 +1,8 @@
 window.nodi = {
     setTheme(dark) {
         document.documentElement.setAttribute('data-theme', dark ? 'dark' : 'light');
+    },
+    focusById(id) {
+        document.getElementById(id)?.focus();
     }
 };
